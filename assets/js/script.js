a609e318e54273d9694da8eaff4de320380b6e81
Maytê <maytearaujo@yahoo.com.br>
2024-04-27 23:26:53 -0300

    Fix: Correção do botão curtir

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 const icon = document.getElementById("icon");
+const icon1 = document.getElementById("icon1");
+const icon2 = document.getElementById("icon2");
 
 icon.onclick = () => {
   icon.classList.toggle("filled");
+};
+
+icon1.onclick = () => {
+  icon1.classList.toggle("filled");
+};
+
+icon2.onclick = () => {
+  icon2.classList.toggle("filled");
 };
